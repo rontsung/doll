1d69eae8d2515f3d9fd82d19183100c82252c172
rontsung <rontsung@gmail.com>
2018-04-30 08:39:48 -0400

    writing the strings to text files

--- Ron_Tsung_mystery.py
+++ Ron_Tsung_mystery.py
@@ -1,18 +1,18 @@
-print r"""
-                   /
-               A.A/
-             ~~ ^ ^\_
- \\         ~~/    __V
-  \\\      ~~/   /
-    \\___>>>     /
-    /             \
-    \      ____/\\\\
-     \ \ \ \    ////
-     / / / /    u u
-     \\  \\
-      u   u
-     """
-print 'Hint: First Code Input: s'
+# print r"""
+#                    /
+#                A.A/
+#              ~~ ^ ^\_
+#  \\         ~~/    __V
+#   \\\      ~~/   /
+#     \\___>>>     /
+#     /             \
+#     \      ____/\\\\
+#      \ \ \ \    ////
+#      / / / /    u u
+#      \\  \\
+#       u   u
+#      """
+# print 'Hint: First Code Input: s'
 import zlib
 import marshal
 import new
@@ -25,9 +25,12 @@ import dis
 import pickle
 
 a0_func = 'YwEAAAAJAAAABAAAAAMAAABzHAEAAGQBAH0BAHQAAGoAAIMAAH0CAHQBAGoCAGoDAIMAAH0DAHQEAGoFAHwDAIMBAH0EAHQGAGoHAHQBAGoCAGoDAIMAAIMBAAF0CAB0AQBqAgBqCQBkAgCDAQCDAQCJAAB0BABqCgB8AwB0BABqCwB8BACDAwABZAMAagwAdA0AhwAAZgEAZAQAhgAAfAAAgwIAgwEAfQUAdAAAagAAgwAAfQYAfAYAfAIAGGQFAGsEAHLVAGQDAGoMAHQNAIcAAGYBAGQGAIYAAHwAAIMCAIMBAH0FAG4AAHwFAGoOAHwBAIMBAH0HAHQPAHwHAIMBAGQCAGsCAHIKAWQHAH0IAHwHAGQIABl8CABmAgBTfAcAZAgAGXwHAGQCABlmAgBTKAkAAABOcwMAAAA6UjppAQAAAHQAAAAAYwEAAAABAAAAAwAAABMAAABzFAAAAHQAAHQBAHwAAIMBAIgAAEGDAQBTKAEAAABOKAIAAAB0AwAAAGNocnQDAAAAb3JkKAEAAAB0AQAAAHgoAQAAAHQKAAAAcGFzc3BocmFzZSgAAAAAcx4AAAAvdXNyL2xvY2FsL2Jpbi9ydXNzaWFuX2RvbGwucHl0CAAAADxsYW1iZGE+LAAAAHMAAAAAaQQAAABjAQAAAAEAAAAEAAAAEwAAAHMYAAAAdAAAdAEAfAAAgwEAiAAAZAEAF0GDAQBTKAIAAABOaQEAAAAoAgAAAFIBAAAAUgIAAAAoAQAAAFIDAAAAKAEAAABSBAAAACgAAAAAcx4AAAAvdXNyL2xvY2FsL2Jpbi9ydXNzaWFuX2RvbGwucHlSBQAAAC8AAABzAAAAAHQIAAAAZmluaXNoZWRpAAAAACgQAAAAdAQAAAB0aW1ldAMAAABzeXN0BQAAAHN0ZGludAYAAABmaWxlbm90BwAAAHRlcm1pb3N0CQAAAHRjZ2V0YXR0cnQDAAAAdHR5dAYAAABzZXRyYXdSAgAAAHQEAAAAcmVhZHQJAAAAdGNzZXRhdHRydAkAAABUQ1NBRFJBSU50BAAAAGpvaW50AwAAAG1hcHQFAAAAc3BsaXR0AwAAAGxlbigJAAAAdAcAAABtZXNzYWdldAwAAABzcGxpdF9zdHJpbmd0AgAAAHQwdAIAAABmZHQMAAAAb2xkX3NldHRpbmdzdAMAAAByZXN0AgAAAHQxUgMAAAB0AQAAAF8oAAAAACgBAAAAUgQAAABzHgAAAC91c3IvbG9jYWwvYmluL3J1c3NpYW5fZG9sbC5weXQKAAAAYXNrZGVjcnlwdCEAAABzIAAAAAABBgIMAQ8BDwEWARgCFgIhAQwBEAEkAQ8BEgEGAQ4B'
-print "base64 a0_func",base64.b64decode(a0_func)[:1000]
+# print "base64 a0_func",base64.b64decode(a0_func)[:1000]
+with open("a0_func.txt", 'w') as ff:
+    ff.write(base64.b64decode(a0_func))
 x = marshal.loads(base64.b64decode(a0_func))
-
+with open("codeobj.txt", 'w') as ff:
+    ff.write(base64.b64decode(x))
 # with open("text.pyc", "wb") as f:
 #   marshal.dump(x,f)
 
@@ -55,6 +58,9 @@ x = zlib.decompress(base64.b64decode('eJzsvU1z40qyJbh/v6KsN/2eaawTpKQrss1mEQACRI
 
 with open("textfile222.txt", "r") as f:
     exec f
+# exec x
+exec "with open('f12.txt', 'w') as ff:+   ff.write(base64.b64decode(base64.b64decode(f12)))"
+exec "with open('xoutput.txt', 'w') as ff:+   ff.write(base64.b64decode(x))"
 # with open("codex.txt", 'w') as ff:
 #   ff.write(x)
 # exec "print dis.dis(x_func)"+x[:x.find("exec")]+x[x.find("t")-1:x.find("except")]+"    "+"print 'worked'"+x[x.find("except")-1:x.find("while")]+"print 'error'"
